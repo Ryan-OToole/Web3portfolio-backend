04c047d6d7fbfd18666addd0094715a97a03db7e
Ryan O'Toole <ryanotoole@Ryans-Air.attlocal.net>
2023-03-16 07:41:20 -0500

    setup env file and cors options

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const corsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  };
+  app.enableCors(corsOptions);
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
