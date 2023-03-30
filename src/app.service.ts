b0e869b50aac8392df641c47e097e017afdb57fe
Ryan O'Toole <ryanotoole@Ryans-Air.attlocal.net>
2023-03-29 19:36:10 -0500

    trying to env variable

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getMetamaskKey(): string {
-    console.log('env', process.env.METAMASK_PRIVATEKEY);
-    return process.env.METAMASK_PRIVATEKEY;
+    console.log('env', METAMASK_PRIVATEKEY);
+    return VERCEL_ENV.METAMASK_PRIVATEKEY;
   }
 }
